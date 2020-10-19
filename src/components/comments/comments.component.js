859214b9f2453148bdfeaaf0186063f15479695e
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2020-10-19 08:33:11 +0300

    Use withData in Comments component

--- src/components/comments/comments.component.js
+++ src/components/comments/comments.component.js
@@ -1,44 +1,23 @@
 import React from 'react';
 
 import { parseDate } from '../../utils';
+import withData from '../../with-data';
 
 import './comments.styles.scss';
 
-class Comments extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      comments: []
+const Comments = ({ data }) => ((
+  <div className="comments">
+    {
+      data.map(comment => (
+        <div className="comments__item" key={comment.id}>
+          <p className="comments__item-name">{comment.name}</p>
+          <p className="comments__item-email">{comment.email}</p>
+          <p className="comments__item-body">"{comment.body}"</p>
+          <p className="comments__date">{parseDate(comment.created_at)}</p>
+        </div>
+      ))
     }
-  }
+  </div>
+));
 
-  componentDidMount() {
-    fetch('https://gorest.co.in/public-api/comments')
-      .then(response => response.json())
-      .then(data => this.setState({ comments: data.data.slice(0, 3) }));
-  }
-
-  render() {
-    return (
-      <div className="comments">
-        {
-          this.state.comments.length
-          ? 
-          this.state.comments.map(comment => (
-            <div className="comments__item" key={comment.id}>
-              <p className="comments__item-name">{comment.name}</p>
-              <p className="comments__item-email">{comment.email}</p>
-              <p className="comments__item-body">"{comment.body}"</p>
-              <p className="comments__date">{parseDate(comment.created_at)}</p>
-            </div>
-          ))
-          : 
-          <p>Loading...</p>
-        }
-      </div>
-    )
-  }
-}
-
-export default Comments;
+export default withData(Comments, 'https://gorest.co.in/public-api/comments');
